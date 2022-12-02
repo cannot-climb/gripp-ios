80e217f12178d00ce9575bb443b6df1e92e97bc7
herojeff02 <herojeff02@gmail.com>
2022-12-02 23:14:33 +0900

    Altered pallette for tier pill

--- gripp-ios/Extension/Color.swift
+++ gripp-ios/Extension/Color.swift
@@ -72,18 +72,64 @@ func getGradient(color1:Color,color2:Color) -> LinearGradient{
 func tierColorProvider(_ tier: Int) -> [Color]{
     switch tier {
     case 1..<5:
-        return [Color("#ffcc00"), Color("#ffe020"), .black]
+        return [Color("#0c991e"), Color("#20a040"), .white] // green
     case 5..<10:
-        return [Color("#337f74"), Color("#206060"), .white]
+        return [Color("#f76806"), Color("#c24e00"), .white] // orange
     case 10..<15:
-        return [Color("#a9ccde"), Color("#c0e0f0"), .black]
-    case 15..<18:
-        return [Color("#dd07f3"), Color("#b000d0"), .white]
+        return [Color("#333f74"), Color("#205080"), .white] // blue
+    case 15..<19:
+        return [Color("#d007f3"), Color("#b000d0"), .white] // purple
     case 19:
-        return [Color("#d0d0ff"), Color("#c0c0d0"), .black]
+        return [Color("#d2ac47"), Color("#edc967"), .black] // gold
+        
+        
     default:
         return [Color(named: "TextMasterColor"), Color(named: "TextMasterColor"), Color(named: "BackgroundMasterColor")]
     }
     
 }
 
+
+
+struct Color_Previews: PreviewProvider {
+    static var previews: some View {
+        HStack{
+            Text("V0").font(.player_tier_pill)
+                .padding(.vertical, 5)
+                .padding(.horizontal, 10)
+                .foregroundColor(tierColorProvider(0)[2])
+                .background(getGradient(color1: tierColorProvider(0)[0], color2: tierColorProvider(0)[1]))
+                .cornerRadius(50, corners: .allCorners)
+            Text("V1").font(.player_tier_pill)
+                .padding(.vertical, 5)
+                .padding(.horizontal, 10)
+                .foregroundColor(tierColorProvider(1)[2])
+                .background(getGradient(color1: tierColorProvider(1)[0], color2: tierColorProvider(1)[1]))
+                .cornerRadius(50, corners: .allCorners)
+            Text("V5").font(.player_tier_pill)
+                .padding(.vertical, 5)
+                .padding(.horizontal, 10)
+                .foregroundColor(tierColorProvider(5)[2])
+                .background(getGradient(color1: tierColorProvider(5)[0], color2: tierColorProvider(5)[1]))
+                .cornerRadius(50, corners: .allCorners)
+            Text("V10").font(.player_tier_pill)
+                .padding(.vertical, 5)
+                .padding(.horizontal, 10)
+                .foregroundColor(tierColorProvider(10)[2])
+                .background(getGradient(color1: tierColorProvider(10)[0], color2: tierColorProvider(10)[1]))
+                .cornerRadius(50, corners: .allCorners)
+            Text("V15").font(.player_tier_pill)
+                .padding(.vertical, 5)
+                .padding(.horizontal, 10)
+                .foregroundColor(tierColorProvider(15)[2])
+                .background(getGradient(color1: tierColorProvider(15)[0], color2: tierColorProvider(15)[1]))
+                .cornerRadius(50, corners: .allCorners)
+            Text("V19").font(.player_tier_pill)
+                .padding(.vertical, 5)
+                .padding(.horizontal, 10)
+                .foregroundColor(tierColorProvider(19)[2])
+                .background(getGradient(color1: tierColorProvider(19)[0], color2: tierColorProvider(19)[1]))
+                .cornerRadius(50, corners: .allCorners)
+        }
+    }
+}
